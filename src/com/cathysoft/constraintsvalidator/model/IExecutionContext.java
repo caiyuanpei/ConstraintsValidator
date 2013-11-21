daa41529b40198747272dded5637f07806e1f39b
Administrator <Administrator@2ZEDMTVB0INCR09>
2013-11-21 17:17:45 +0800

    daily commit

--- src/com/cathysoft/constraintsvalidator/model/IExecutionContext.java
+++ src/com/cathysoft/constraintsvalidator/model/IExecutionContext.java
@@ -2,8 +2,6 @@ package com.cathysoft.constraintsvalidator.model;
 
 public interface IExecutionContext {
 	
-	public boolean containsClassWithName(Class<?> clazz, String instanceName);
-	
 	
 
 }
--- src/com/cathysoft/constraintsvalidator/model/spi/MappedExecutionContext.java
+++ src/com/cathysoft/constraintsvalidator/model/spi/MappedExecutionContext.java
@@ -9,17 +9,5 @@ public class MappedExecutionContext implements IExecutionContext {
 
 	public Map<String, Object> objectPool = new HashMap<String, Object>();
 	
-	@Override
-	public boolean containsClassWithName(Class<?> clazz, String instanceName) {
-		
-		if(!objectPool.containsKey(instanceName))
-			return false;
-		
-		Object obj = objectPool.get(instanceName);
-		if(!obj.getClass().equals(clazz))
-			return false;
-		
-		return true;
-	}
-
+	
 }
